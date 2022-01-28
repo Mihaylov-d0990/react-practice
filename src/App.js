fc66e85efd43cfb1c04fbec52b5875e7eac98122
Danila <syvs0990@mail.ru>
2022-01-28 22:37:14 +0700

    Moved styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import ToDoList from "./todolist/ToDoList";
-import './style/style.css'
+
 
 function App() {
     return (
--- src/todolist/ToDoList.jsx
+++ src/todolist/ToDoList.jsx
@@ -1,3 +1,5 @@
+import './style/style.css'
+
 import React from 'react'
 import List from './List'
 
