4a804b0cd9a3499998d23765920dd44436244496
Danila <syvs0990@mail.ru>
2022-01-28 17:36:21 +0700

    ToDoList: Added input and button and added functions for them

--- src/todolist/List.jsx
+++ src/todolist/List.jsx
@@ -3,7 +3,7 @@
 export default function List({issues}) {
 
     return(
-        <ul className="list-non-styled">
+        <ul className="list-non-styled padding-top-25">
             {issues.map((issue, index) => (
                 <li key={index}>{issue}</li>
             ))}
--- src/todolist/ToDoList.jsx
+++ src/todolist/ToDoList.jsx
@@ -3,9 +3,20 @@ import List from './List'
 
 export default function ToDoList() {
     const [issues, setIssues] = React.useState(['First issue', 'Second issue', 'Third issue'])
+    const [inputIssue, setInputIssue] = React.useState("")
+
+    const addIssue = () => {
+        setIssues([...issues, inputIssue])
+    }
+
+    const issueChangeHandler = (e) => {
+        setInputIssue(e.target.value) 
+    }
 
     return (
         <div className='container'>
+            <input type="text" placeholder="Enter your issue" value={inputIssue} onChange={issueChangeHandler}/>
+            <input type="button" value="Add issue" onClick={addIssue}/>
             <List issues={issues} />
         </div>
     )
