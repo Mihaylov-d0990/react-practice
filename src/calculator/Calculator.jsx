98ba313152462afeb6712962d39895b1f712e8d2
Danila <syvs0990@mail.ru>
2022-01-29 21:59:57 +0700

    Fix calculations

--- src/calculator/Calculator.jsx
+++ src/calculator/Calculator.jsx
@@ -2,29 +2,50 @@ import React from 'react'
 import CalcButton from './CalcButton'
 
 export default function Calculator() {
-    const operators = new Set(["*", "/", "+", "-"])
 
-    const [buttons, setButtons] = React.useState(() => {
+    const buttons = React.useState(() => {
         let arr = []
         for (let i = 1; i < 10; i++){
             arr[i-1] = i.toString()
         }
         arr = [...arr, ...["0", "+", "-", "*", "/"]]
         return arr
-    })
+    })[0]
 
     const [expression, setExpression] = React.useState('')
     
     const [operationResult, setOperationResult] = React.useState('')
 
     const addSymbol = (symbol) => {
+        const operators = new Set(["*", "/", "+", "-"])
         let exp = expression
-        
-        if (operators.has(expression[expression.length - 1]) && operators.has(symbol)) {
-            exp = exp.substring(0, expression.length - 1) 
+
+        if (operators.has(symbol)) {
+
+            if (exp.length === 0 && symbol === "-") {
+
+                setExpression(exp + symbol)
+                
+            } else if (operators.has(exp[exp.length - 1])) {  
+
+                if (exp[0] !== "-" && exp[exp.length - 1] !== "-") {
+
+                    exp = exp.substring(0, exp.length - 1) 
+                    setExpression(exp + symbol)
+
+                } else if (exp.length > 1) {
+
+                    exp = exp.substring(0, exp.length - 1) 
+                    setExpression(exp + symbol)
+                }
+                
+            } else if (exp.length >= 1) {
+                setExpression(exp + symbol)
+            }
+            
+        } else {
+            setExpression(exp + symbol)
         }
-        
-        setExpression(exp + symbol)
     }
 
     const deleteSymbol = () => {
@@ -40,6 +61,7 @@ export default function Calculator() {
         let res
         let f = Number(fNum)
         let s = Number(sNum)
+
         switch(op){
             case "+":
                 res = f + s
@@ -53,49 +75,62 @@ export default function Calculator() {
             case "/":
                 res = f / s
             break;
+            default:
+                res = ""
+            break;
         }
         return res.toString()
     }
 
-    const combine = (expArr) => {
-
-        let arrLength = expArr.length
-        if (arrLength % 2 === 1 && arrLength > 2) {
-
-            [...operators].map((op) => {               
-                for (let i = 0; i < expArr.length; i++) {
-                    if (expArr[i] === op) {    
-                        expArr.splice(i-1, 3, result(expArr[i-1], expArr[i+1], expArr[i]))
-                        return combine(expArr)
-                    }
-                }
-            }) 
-            return expArr
-
-        } else {
-
-            return [...expArr]
-
-        }
-        
-    }
+    
  
     React.useEffect(() => {
-        let exp = expression.split('')
+
+        const operators = new Set(["*", "/", "+", "-"])
+
+        const combine = (expArr) => {
+            let arrLength = expArr.length
+            if (arrLength % 2 === 1 && arrLength > 2) {
+    
+                [...operators].map((op) => {               
+                    for (let i = 0; i < expArr.length; i++) {
+                        if (expArr[i] === op) {    
+                            expArr.splice(i-1, 3, result(expArr[i-1], expArr[i+1], expArr[i]))
+                            return combine(expArr)
+                        }
+                    }
+                    return expArr
+                }) 
+                return expArr
+            } else {
+                return [...expArr]
+            }       
+        }
+
+        let exp = expression.split('')        
         let expressionArray = []
         exp.map((symbol) => {
+
             if (operators.has(symbol)) {
+
                 expressionArray = [...expressionArray, symbol]
+
             } else {
+
                 let arrayLength = expressionArray.length
                 if ((arrayLength !== 0 && !operators.has(expressionArray[arrayLength - 1])) ||
                     (arrayLength === 1 && !(new Set("*", "/", "+", "-")).has(expressionArray[arrayLength - 1]))
                 ) {
+
                     expressionArray[arrayLength - 1] = expressionArray[arrayLength - 1] + symbol
+
                 } else {
+
                     expressionArray = [...expressionArray, symbol]
+                    
                 }
             }
+            return expressionArray
         })
 
         let ea = [...expressionArray]
